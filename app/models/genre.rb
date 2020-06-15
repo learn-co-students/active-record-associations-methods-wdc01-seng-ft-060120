ea1f6c3e4e300499fedd94f066c8bd6fc15579fc
Issa sangare <issabmsangare@hotmail.com>
2020-06-14 23:47:39 -0400

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,23 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    total_songs = Song.all.select {|song| song.genre = self}
+    number_of_songs = total_songs.count
+    number_of_songs
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    total_songs = Song.all.select {|song| song.genre = self}
+    total_artist = total_songs.map {|song| song.artist}
+    number_of_artists = total_artist.count
+    number_of_artists
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    total_songs = Song.all.select {|song| song.genre = self}
+    artist_names = total_songs.map {|song| song.artist.name}
+    artist_names
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -3,12 +3,19 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
+
+    songs = Song.all.select {|song| song == self}
+    genres_name = songs.map {|song| song.genre.name}
+    genres_name.join
   end
 
   def drake_made_this
     # when this method is called it should assign the song's artist to Drake
     # Drake doesn't exist in the database as an artist yet, so you'll have to create a record
     # Hint: you won't want to create an artist record every time this method is called, only if an Drake is *not found*
-
+    drake = Artist.find_or_create_by(name: "Drake")
+    drake.songs << self
   end
 end
+
+
